c65d417a6039ba8ce1fb78cf356d93192503c56e
virgostyx <virgostyx@gmail.com>
2026-05-16 10:27:25 +0200

    Add user registration page, seed script, and "create account" links
    - Add `new.html.erb` for user registrations with Tailwind CSS styling
    - Add `UsersSeeder` in `db/seeders` to create test users
    - Update `db/seeds.rb` to call `UsersSeeder`
    - Add "Pas encore de compte ? Créer un compte" links to session and registration views for navigation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require_relative 'seeders/pcmn_seeder'
 require_relative 'seeders/journals_seeder'
+require_relative 'seeders/users_seeder'
 
 Seeders::PcmnSeeder.call
 Seeders::JournalsSeeder.call
+Seeders::UsersSeeder.call
